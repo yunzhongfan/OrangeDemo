1183f76401dd1583b98088ebc85ffd851d22b92c
yunzhongfan <763748729@qq.com>
2017-12-07 11:05:24 +0800

    增加了JDK自带的代理类  propxy和reflect

---
+++ practise/javademo/src/main/java/org/base/reflect/Myclass.java
@@ -0,0 +1,29 @@
+package org.base.reflect;
+
+public class Myclass {
+	
+	private String data1="我是父类的data1属性";
+	
+	public String data2="我是父类的data2属性";
+	
+	
+	public void getTestClassMethod() {
+		System.out.println("Myclass.getTestClassMethod，代理方法1");
+	}
+	public void getTestClassMethod(String str) {
+		System.out.println("Myclass.getTestClassMethod1，代理方法2");
+	}
+	
+	public void getTestClassMethod(String str,String str1) {
+		System.out.println("Myclass.getTestClassMethod2，代理方法3");
+	}
+	
+	public void  myclassOne(){
+		System.out.println("myclassOne");
+	}
+	
+	public void  myclassTwo(){
+		System.out.println("myclassTwo");
+	}
+
+}
--- practise/springMVCdemo/src/main/java/demo1/UserController.java
+++ practise/springMVCdemo/src/main/java/demo1/UserController.java
@@ -9,21 +9,38 @@
 package demo1;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-/** 
-* @ClassName: UserController 
-* @Description: TODO
-* @author nbc
-* @date 2017年12月5日 下午4:13:51 
-*  
-*/
+/**
+ * @ClassName: UserController
+ * @Description: TODO
+ * @author nbc
+ * @date 2017年12月5日 下午4:13:51
+ * 
+ */
 
-@RestController
-@RequestMapping(path="/user",value="",produces=MediaType.APPLICATION_JSON_UTF8_VALUE,consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Controller
+@RequestMapping(path = "/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class UserController {
-	
-	
+
+	static {
+		System.out.println("springMVC启动了！");
+	}
+
+	@RequestMapping(path = "/user/*/hello")
+	public String hello() {
+		return "hello";
+	}
+
+	@RequestMapping("/{userId}")
+	public ModelAndView showDetail(@PathVariable("userId") String userId) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("user/showDetail");
+		// mav.addObject("user", userService.getUserById(userId));
+		return mav;
+	}
 
 }
